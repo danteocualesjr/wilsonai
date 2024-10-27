ad84d13daf7a032632059ae9dca8139266587a47
danteocualesjr <danteocualesjr@gmail.com>
2024-10-27 17:03:28 +0800

    updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-import WilsonAI from '@/components/WilsonAI'
+import dynamic from 'next/dynamic'
+
+// Dynamically import WilsonAI with no SSR
+const WilsonAI = dynamic(() => import('@/components/WilsonAI'), { ssr: false })
 
 export default function Home() {
   return <WilsonAI />
